f0a04b07f4813e6020749c3858631020e7a279e6
oscarperez.1985 <oscarperez.1985@yahoo.com.mx>
2022-11-21 00:18:39 -0600

    Deliverables 1 2 and 3 complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,8 +1,8 @@
-# DELIVERABLE 1:
-
 # Import libraries
 library(dplyr)
 library(tidyverse)
+
+# DELIVERABLE 1:
 
 # Input Data
 mecha_mpg <- read.csv(file='./Data/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) 
@@ -33,4 +33,19 @@ lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PS
                                                                          Std_Dev_PSI=sd(PSI),
                                                                          Num_Coil=n(), .groups = 'keep')
 
+# Deliverable 3
 
+# T-test to determine if the PSI across all manufacturing lots is statistically
+# different from the population mean of 1,500 pounds per square inch.
+t.test(mecha_coil$PSI,mu=1500)
+
+# Determine if the PSI for each manufacturing lot is statistically different
+# from the population mean of 1,500 pounds per square inch
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+t.test(lot1$PSI,mu=1500)
+
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+t.test(lot2$PSI,mu=1500)
+
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+t.test(lot3$PSI,mu=1500)
